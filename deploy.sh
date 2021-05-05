29e267a1955e30435f13e41793302cad3e4e697a
Weentry <flaky12r@mail.ru>
2021-05-05 21:01:28 +0500

    ferst commit

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+npm run build
+
+# переход в каталог сборки
+cd dist
+
+# если вы публикуете на пользовательский домен
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:Weenty/impromptu-social-network.git master:gh-pages
+
+cd -
